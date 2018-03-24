3770c7d93042bd4d23eb3604bae51d6316dda575
Spyros Bartokaymenos <tp4417@edu.teicrete.gr>
2018-03-24 17:32:44 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,7 +1,10 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {},
+      :patriarch => {
+        :name => "Lord Montague",
+        :age => "53"
+      },
       :matriarch => {},
       :hero => {},
       :hero_friends => []
@@ -13,5 +16,5 @@ def second_challenge
       :heroine_friends => []
       }}
 
-
+      
 end
