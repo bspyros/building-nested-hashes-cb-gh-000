a496adf66acbd3127d25d315b939a75a38ec0ef7
Spyros Bartokaymenos <tp4417@edu.teicrete.gr>
2018-03-24 17:34:33 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -17,7 +17,10 @@ def second_challenge
       :hero_friends => []
       },
     :capulet => {
-      :patriarch => {},
+      :patriarch => {
+        :name => "Lord Capulet",
+        :age => "50"
+      },
       :matriarch => {},
       :heroine => {},
       :heroine_friends => []
