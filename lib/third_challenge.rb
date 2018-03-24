d3f6fb7b1c0525791f10433b81402a75ab1cc966
Spyros Bartokaymenos <tp4417@edu.teicrete.gr>
2018-03-24 18:25:43 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -13,32 +13,4 @@ def third_challenge
       :heroine_friends => []
    }
 }
-  epic_tragedy = {
-   :montague => {
-      :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :hero => {
-        #your key/value pairs here
-        },
-      :hero_friends => []
-   }, 
-   :capulet => {
-      :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :heroine => {
-        #your key/value pairs here
-        },
-      :heroine_friends => []
-   }
-  }
-
-  
 end
